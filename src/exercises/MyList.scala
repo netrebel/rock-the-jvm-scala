52dcde79795dd74d3e1a97bf03d2be540f8515d3
Miguel Reyes <mreyes@life360.com>
2019-05-13 13:48:10 -0700

    MyList implementation (abstract class)

---
+++ src/exercises/MyList.scala
@@ -0,0 +1,53 @@
+package exercises
+
+abstract class MyList {
+
+  def head: Int
+
+  def tail: MyList
+
+  def isEmpty: Boolean
+
+  def add(n: Int): MyList
+
+  def printElements: String
+
+  override def toString: String = "[" + printElements + "]"
+
+}
+
+object Empty extends MyList {
+  override def head: Int = throw new NoSuchElementException
+
+  override def tail: MyList = throw new NoSuchElementException
+
+  override def isEmpty: Boolean = true
+
+  override def add(n: Int): MyList = new Cons(n, Empty)
+
+  override def printElements: String = ""
+
+}
+
+class Cons(h: Int, t: MyList) extends MyList {
+  override def head: Int = h
+
+  override def tail: MyList = t
+
+  override def isEmpty: Boolean = false
+
+  override def add(n: Int): MyList = new Cons(n, this)
+
+  override def printElements: String =
+    if (t.isEmpty) "" + h
+    else h + " " + t.printElements
+}
+
+object ListTest extends App {
+  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  println(list.tail.head)
+  println(list.add(4).head)
+  println(list.isEmpty)
+
+  println(list.toString)
+}
